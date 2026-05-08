959ef4089b9cbb584894c911e9002654c42dffb5
Othman-Darkal20752 <odsoftware4@gmail.com>
2026-05-08 20:15:57 +0300

    Revert "Test1"

--- AI_ChatBot_service/ChatBot/utils/user_headers.py
+++
@@ -1,8 +0,0 @@
-def get_user_from_headers(request):
-    headers = request.META
-
-    return {
-        "student_id": headers.get("HTTP_X_STUDENT_ID"),
-        "user_id": headers.get("HTTP_X_USER_ID"),
-        "username": headers.get("HTTP_X_USERNAME"),
-    }
--- Quiz_Generator_service/core/__init__.py
+++
@@ -1,1 +0,0 @@
-# Init core module
